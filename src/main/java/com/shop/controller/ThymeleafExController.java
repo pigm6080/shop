64d8ebf7d51694112735464b02d5e0bb8d9c3f61
pigm6080 <pigm6080@gmail.com>
2024-04-03 23:25:47 +0900

    타입 리프 ex02

--- src/main/java/com/shop/controller/ThymeleafExController.java
+++ src/main/java/com/shop/controller/ThymeleafExController.java
@@ -1,9 +1,14 @@
 package com.shop.controller;
 
+import com.shop.dto.ItemDto;
+import com.shop.entity.Item;
+import org.springframework.boot.Banner;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.time.LocalDateTime;
 
 @RequestMapping("/thymeleaf")
 @Controller
@@ -13,5 +18,18 @@ public class ThymeleafExController {
     public String thymeleafExample01(Model model){
         model.addAttribute("data","타입리프 예제입니다.");
         return "thymeleafEx/thymeleafEx01";
+    }
+
+    @GetMapping("/ex02")
+    public String thymeleafExample02(Model model){
+        ItemDto itemDto = new ItemDto();
+        itemDto.setItemDetail("상품 상세 설명");
+        itemDto.setItemNm("테스트 상품1");
+        itemDto.setPrice(10000);
+        itemDto.setRegTime(LocalDateTime.now());
+
+        model.addAttribute("itemDto",itemDto);
+
+        return "thymeleafEx/thymeleafEx02";
     }
 }
