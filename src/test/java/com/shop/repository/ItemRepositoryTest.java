929a9580a0d0539403cdab16a4a2532c20877495
pigm6080 <pigm6080@gmail.com>
2024-04-03 10:30:34 +0900

    insert test 완료

--- src/test/java/com/shop/repository/ItemRepositoryTest.java
+++ src/test/java/com/shop/repository/ItemRepositoryTest.java
@@ -1,7 +1,35 @@
 package com.shop.repository;
 
+import com.shop.constant.ItemSellStatus;
+import com.shop.entity.Item;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDateTime;
+
+@SpringBootTest
 public class ItemRepositoryTest {
 
-    
+    @Autowired
+    ItemRepository itemRepository;
+    @Test
+    @DisplayName("상품 저장 테스트")
+    public void createItemTest() {
+        Item item = Item.builder()
+                .itemNm("테스트 상품")
+                .price(10000)
+                .itemDetail("테스트 상품 상세 설명")
+                .itemSellStatus(ItemSellStatus.SELL)
+                .stockNumber(100)
+                .regTime(LocalDateTime.now())
+                .updateTime(LocalDateTime.now())
+                .build();
+        System.out.println("=========> item " + item);
+        Item saveItem = itemRepository.save(item);
+        System.out.println("=========> item " + saveItem);
+    }
+
 
 }
