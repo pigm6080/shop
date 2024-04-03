fb06c96b5028ed9a63d498e5eea48e2291b12200
pigm6080 <pigm6080@gmail.com>
2024-04-03 23:11:36 +0900

    타입 리프 ex01

--- src/main/java/com/shop/controller/ThymeleafExController.java
+++ src/main/java/com/shop/controller/ThymeleafExController.java
@@ -1,4 +1,17 @@
 package com.shop.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/thymeleaf")
+@Controller
 public class ThymeleafExController {
+
+    @GetMapping("/ex01")
+    public String thymeleafExample01(Model model){
+        model.addAttribute("data","타입리프 예제입니다.");
+        return "thymeleafEx/thymeleafEx01";
+    }
 }
