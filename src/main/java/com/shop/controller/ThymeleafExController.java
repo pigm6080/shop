198e1c96ca7aa4cdd0e8cef5a6582f4292f053e7
pigm6080 <pigm6080@gmail.com>
2024-04-03 23:48:14 +0900

    타입 리프 ex03

--- src/main/java/com/shop/controller/ThymeleafExController.java
+++ src/main/java/com/shop/controller/ThymeleafExController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @RequestMapping("/thymeleaf")
 @Controller
@@ -31,5 +33,26 @@ public class ThymeleafExController {
         model.addAttribute("itemDto",itemDto);
 
         return "thymeleafEx/thymeleafEx02";
+    }
+
+    @GetMapping("/ex03")
+    public String thymeleafExample03(Model model){
+
+        List<ItemDto> itemDtoList = new ArrayList<>();
+
+        for(int i = 1; i<=10;i++){
+
+            ItemDto itemDto = new ItemDto();
+            itemDto.setItemDetail("상품 상세 설명");
+            itemDto.setItemNm("테스트 상품" + i);
+            itemDto.setPrice(1000*i);
+            itemDto.setRegTime(LocalDateTime.now());
+
+            itemDtoList.add(itemDto);
+        }
+
+        model.addAttribute("itemDtoList", itemDtoList);
+
+        return "thymeleafEx/thymeleafEx03";
     }
 }
