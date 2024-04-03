16ce5b3010a15ddbfa751d432ad3e77fa0454d3e
pigm6080 <pigm6080@gmail.com>
2024-04-03 10:42:37 +0900

    select test 완료

--- src/test/java/com/shop/repository/ItemRepositoryTest.java
+++ src/test/java/com/shop/repository/ItemRepositoryTest.java
@@ -8,12 +8,47 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @SpringBootTest
 public class ItemRepositoryTest {
 
     @Autowired
     ItemRepository itemRepository;
+
+    public void createItemList(){
+        for(int i = 1; i<=10; i++){
+            Item item = Item.builder()
+                    .itemNm("테스트 상품" + i)
+                    .price(10000 + i)
+                    .itemDetail("테스트 상품 상세 설명" + i)
+                    .itemSellStatus(ItemSellStatus.SELL)
+                    .stockNumber(100)
+                    .regTime(LocalDateTime.now())
+                    .updateTime(LocalDateTime.now())
+                    .build();
+
+            Item saveItem = itemRepository.save(item);
+        }
+    }
+
+    @Test
+    @DisplayName("상품 조회 테스트")
+    public void findByItemTest() {
+        this.createItemList(); //데이터 10개 저장
+        List<Item> itemList = itemRepository.findByItemNm("테스트 상품1");
+    /*
+        for (Item item : itemList){
+            System.out.println(item.toString());
+        }
+    */
+        itemList.forEach(item -> System.out.println(item));
+
+
+    }
+
+
+
     @Test
     @DisplayName("상품 저장 테스트")
     public void createItemTest() {
@@ -30,6 +65,8 @@ public class ItemRepositoryTest {
         Item saveItem = itemRepository.save(item);
         System.out.println("=========> item " + saveItem);
     }
+
+
 
 
 }
