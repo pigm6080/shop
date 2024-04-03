c7fba86e3ae6e86cc949d7492e521bb87740c0d4
pigm6080 <pigm6080@gmail.com>
2024-04-03 11:39:26 +0900

    findByItemDetailByNative 완료

--- src/test/java/com/shop/repository/ItemRepositoryTest.java
+++ src/test/java/com/shop/repository/ItemRepositoryTest.java
@@ -31,6 +31,20 @@ public class ItemRepositoryTest {
             Item saveItem = itemRepository.save(item);
         }
     }
+
+    @Test
+    @DisplayName("nativeQuery 속성을 이용한 상품 조회 테스트")
+    public void findByItemDetailByNative() {
+        this.createItemList(); //데이터 10개 저장
+        List<Item> itemList = itemRepository.findByItemDetail("테스트 상품 상세 설명");
+    /*
+        for (Item item : itemList){
+            System.out.println(item.toString());
+        }ㄴ
+    */
+        itemList.forEach(item -> System.out.println(item));
+    }
+
     @Test
     @DisplayName("@Query를 이용한 상품 조회 테스트")
     public void findByItemDetailTest() {
