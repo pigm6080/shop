6842dbbb55fd33648d41716df5bab120e2287417
pigm6080 <pigm6080@gmail.com>
2024-04-03 11:32:51 +0900

    JPQL 은 쓰지말자.

--- src/main/java/com/shop/repository/ItemRepository.java
+++ src/main/java/com/shop/repository/ItemRepository.java
@@ -2,6 +2,8 @@ package com.shop.repository;
 
 import com.shop.entity.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -14,5 +16,12 @@ public interface ItemRepository extends JpaRepository<Item, Long> {
     List<Item> findByPriceLessThan(Integer price);
 
     List<Item> findByPriceLessThanOrderByPriceDesc(Integer price);
+
+    @Query("select i from Item i where i.itemDetail like %:itemDetail% order by i.price desc") //객체로 한것이다.
+    List<Item> findByItemDetail(@Param("itemDetail") String itemDetail); //객체로 한것
+
+    @Query(value = "select * from item i where i.item_detail like %:itemDetail% order by i.price desc", nativeQuery = true) //데이터베이스로함
+    List<Item> findByItemDetailByNative(@Param("itemDetail") String itemDetail); //데이터 베이스 noaml한것. , nativeQuery =true 이반적인 db쿼리쓰겠다.
+    //: <-- 은 변수로 받아달라.
 
 }
