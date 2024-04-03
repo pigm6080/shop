5b1dcc845d62a31ee142c473b2f3acb9f3e7a7cf
pigm6080 <pigm6080@gmail.com>
2024-04-03 11:59:44 +0900

    queryDSL 테스트 완료

--- src/test/java/com/shop/repository/ItemRepositoryTest.java
+++ src/test/java/com/shop/repository/ItemRepositoryTest.java
@@ -1,7 +1,10 @@
 package com.shop.repository;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.shop.constant.ItemSellStatus;
 import com.shop.entity.Item;
+import com.shop.entity.QItem;
+import jakarta.persistence.EntityManager;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,6 +18,32 @@ public class ItemRepositoryTest {
 
     @Autowired
     ItemRepository itemRepository;
+
+    @Autowired
+    EntityManager em; //queryDSL
+
+    @Test
+    @DisplayName("Querydsl 조회 테스트1")
+    public void queryDslTest(){
+        this.createItemList();
+        JPAQueryFactory query = new JPAQueryFactory(em);
+        QItem qItem = QItem.item;
+
+        List<Item> itemList = query.selectFrom(QItem.item)
+                .where(qItem.itemSellStatus.eq(ItemSellStatus.SELL))
+                .where(qItem.itemDetail.like("%" + "테스트 상품 상세 설명" + "%"))
+                .orderBy(qItem.price.desc())
+                .fetch();
+/*
+		for(Item item : itemList){
+			System.out.println(item.toString());
+		}
+ 		*/
+        itemList.forEach(item -> System.out.println(item));
+
+    }
+
+
 
     public void createItemList(){
         for(int i = 1; i<=10; i++){
@@ -31,6 +60,9 @@ public class ItemRepositoryTest {
             Item saveItem = itemRepository.save(item);
         }
     }
+
+
+
 
     @Test
     @DisplayName("nativeQuery 속성을 이용한 상품 조회 테스트")
