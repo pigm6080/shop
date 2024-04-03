0babf494d36c4ef256a59594e12f11f40ae63504
pigm6080 <pigm6080@gmail.com>
2024-04-03 10:55:29 +0900

    findByPriceLessThanTest 완료

--- src/test/java/com/shop/repository/ItemRepositoryTest.java
+++ src/test/java/com/shop/repository/ItemRepositoryTest.java
@@ -33,6 +33,19 @@ public class ItemRepositoryTest {
     }
 
     @Test
+    @DisplayName("가격 LessThan 테스트")
+    public void findByPriceLessThanTest() {
+        this.createItemList(); //데이터 10개 저장
+        List<Item> itemList = itemRepository.findByPriceLessThan(10005);
+    /*
+        for (Item item : itemList){
+            System.out.println(item.toString());
+        }
+    */
+        itemList.forEach(item -> System.out.println(item));
+    }
+
+    @Test
     @DisplayName("상품명, 상품 상세 설명 or 테스트")
     public void findByItemNmOrItemDetailTest() {
         this.createItemList(); //데이터 10개 저장
