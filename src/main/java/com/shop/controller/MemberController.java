2e2904ab20003bfc06ac75b7f413c7eccf96e945
pigm6080 <pigm6080@gmail.com>
2024-04-13 10:13:32 +0900

    시큐리티 벨리데이션 완료

--- src/main/java/com/shop/controller/MemberController.java
+++ src/main/java/com/shop/controller/MemberController.java
@@ -1,11 +1,16 @@
 package com.shop.controller;
 
 import com.shop.dto.MemberFormDto;
+import com.shop.entity.Member;
 import com.shop.service.MemberService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/members")
@@ -14,10 +19,23 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class MemberController {
 
     private final MemberService memberService;
+    private final PasswordEncoder passwordEncoder;
 
     @GetMapping("/new")
     public String memberForm(Model model){
         model.addAttribute("memberFormDto",new MemberFormDto());
-        return "member/memberForm";
+        return "members/memberForm";
+    }
+    @PostMapping("/new")
+    public String memberForm(@Valid MemberFormDto memberFormDto,
+                             BindingResult bindingResult, Model model){
+
+        if(bindingResult.hasErrors()){
+            return "members/memberForm";
+        }
+        Member member = Member.createMember(memberFormDto, passwordEncoder);
+        memberService.saveMember(member);
+
+        return "redirect:/";
     }
 }
--- src/main/java/com/shop/dto/MemberFormDto.java
+++ src/main/java/com/shop/dto/MemberFormDto.java
@@ -1,6 +1,10 @@
 package com.shop.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.*;
+import org.hibernate.validator.constraints.Length;
 
 @Getter
 @Setter
@@ -10,12 +14,18 @@ import lombok.*;
 @Builder
 public class MemberFormDto { //가입화면으로 부터 넘어오는 값을 감을 dto생성.
 
+    @NotBlank( message = "이름은 필수 입력 값입니다.")
     private String name;
 
+    @NotEmpty( message = "이멜일은 필수 입력 값입니다.")
+    @Email( message = "이메일 형식으로 입력해 주세요.")
     private String email;
 
+    @NotEmpty( message = "비밀번호는 필수 입력 값입니다.")
+    @Length(min=8,max=16,message = "비밀번호는 8자 이상, 16자 이하로 입력해주세요.")
     private String password;
 
+    @NotEmpty( message = "주소는 필수 입력 값입니다. ")
     private String address;
 
 }
